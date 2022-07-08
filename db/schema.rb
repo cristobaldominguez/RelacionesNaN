4cb7fbda4f716346811c34ea2ce010271cc217e1
Cristobal Dominguez <cristobald@gmail.com>
2022-07-08 18:37:57 -0400

    Agrega Scaffold de Tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_08_223450) do
+ActiveRecord::Schema.define(version: 2022_07_08_223717) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.string "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
